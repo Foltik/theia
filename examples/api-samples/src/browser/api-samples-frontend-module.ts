89507d779b892f81f94313a43a0bec67fd720820
Akos Kitta <kittaakos@typefox.io>
2020-10-08 22:34:16 +0200

    Sample: default HTML
    See: community.theia-ide.org/t/by-default-open-html-file-in-preview/1080
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -19,12 +19,54 @@ import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
 import { bindSampleOutputChannelWithSeverity } from './output/sample-output-channel-with-severity';
 import { bindSampleMenu } from './menu/sample-menu-contribution';
+import { inject, injectable } from 'inversify';
+import URI from '@theia/core/lib/common/uri';
+import { EditorManager } from '@theia/editor/lib/browser';
+import { MiniBrowser } from '@theia/mini-browser/lib/browser/mini-browser';
+import { MiniBrowserOpenHandler } from '@theia/mini-browser/lib/browser/mini-browser-open-handler';
+import { FrontendApplication, FrontendApplicationContribution } from '@theia/core/lib/browser/frontend-application';
 
 import '../../src/browser/style/branding.css';
 
-export default new ContainerModule(bind => {
+export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bindDynamicLabelProvider(bind);
     bindSampleUnclosableView(bind);
     bindSampleOutputChannelWithSeverity(bind);
     bindSampleMenu(bind);
+    bind(MyMiniBrowserOpenHandler).toSelf().inSingletonScope();
+    rebind(MiniBrowserOpenHandler).toService(MyMiniBrowserOpenHandler);
+    bind(FrontendApplicationContribution).to(MyCustomFrontendContribution).inSingletonScope();
 });
+
+@injectable()
+class MyMiniBrowserOpenHandler extends MiniBrowserOpenHandler {
+
+    canHandle(uri: URI): number {
+        // Note: `HtmlHandler` is not bound `toSelf` so we customize the behavior here and not on the backend.
+        if (uri.scheme === 'file' && uri.path.ext === '.html') {
+            const CODE_EDITOR_PRIORITY = 100; // See: `EditorManager#canHandle` in packages/editor/src/browser/editor-manager.ts
+            return CODE_EDITOR_PRIORITY * 2;
+        }
+        return super.canHandle(uri);
+    }
+
+}
+
+@injectable()
+class MyCustomFrontendContribution implements FrontendApplicationContribution {
+
+    @inject(EditorManager)
+    protected editorManager: EditorManager;
+
+    onStart(app: FrontendApplication): void {
+        app.shell.onDidAddWidget(widget => {
+            if (widget instanceof MiniBrowser) {
+                const uri = widget.getResourceUri();
+                if (uri && uri.scheme === 'file' && uri.path.ext === '.html') {
+                    this.editorManager.open(uri, { widgetOptions: { ref: widget, mode: 'open-to-right' } });
+                }
+            }
+        });
+    }
+
+}
